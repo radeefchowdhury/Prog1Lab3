605e8102003586edb808b7de39c3e91f8ab2142d
radeefchowdhury <60351584+radeefchowdhury@users.noreply.github.com>
2020-03-14 21:23:21 -0400

    Update BmiCalculator.java

--- BmiCalculator.java
+++ BmiCalculator.java
@@ -1,17 +1,24 @@
 import java.util.*;
 public class BmiCalculator {
+    //This calculates ur BMI, telling you if youre healthy, obese, or underweight
     public static void main(String[] args) {
+        
+        //This converts feet into meters
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your height in feet");
         double heightfeet = sc.nextDouble();
          final double meters = 0.3048 *  heightfeet;
         
+        //This converts pounds to kg
         System.out.println("Enter your weight in pounds");
         double weightpounds = sc.nextDouble();
          final double kilograms = weightpounds *0.453592;
         
+        //This calculates the BMI
         double bmi = kilograms/ (Math.pow(meters,2)); 
         System.out.println("Your BMI is:" + bmi);
+        
+        //This sees if your bmi and displays a message accordingly using if/else
         if(bmi<18.5){
            System.out.println("You are underweight");
         }
